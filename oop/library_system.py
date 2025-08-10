80ca22bfe9859738eb433c0541e4f7b3f4c832ae
Melik Yunus <melikact34@gmail.com>
2025-08-10 15:54:15 +0300

    second commit for library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -25,7 +25,7 @@ class PrintBook(Book):
         self.page_count = page_count
 
     def __str__(self):
-        return f"PrintBook: {self.title} by {self.author}, pagecount: {self.page_count}"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
 class Library:
